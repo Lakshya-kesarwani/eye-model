a2f81358d1f9f31bb047d233547fcec07dd95130
Lakshya Kesarwani <ansh1.kesari2005@gmail.com>
2025-06-05 16:15:19 +0530

    Implement code changes to enhance functionality and improve performance

--- Home.tsx
+++ Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet,Image } from 'react-native';
 import App from './pages/App';
 import App2 from './pages/App2';
 
@@ -11,6 +11,8 @@ const App3 = () => {
 
   return (
     <View style={styles.container}>
+      {/* <Image src='./assets/eye.png' ></Image> */}
+      <Image source={require('./assets/eye.png')} style={{ width: 300, height: 200, marginBottom: 20 }} />
       <Text style={styles.title}>Select App Mode</Text>
       <TouchableOpacity style={styles.button} onPress={() => setMode('app1')}>
         <Text style={styles.buttonText}>Create Dataset</Text>
@@ -23,7 +25,7 @@ const App3 = () => {
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' ,backgroundColor: '#f0f8ff'},
   title: { fontSize: 28, marginBottom: 40, fontWeight: 'bold' },
   button: {
     backgroundColor: '#4682B4',
